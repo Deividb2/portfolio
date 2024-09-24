a1b03aac357877c03e71e0227e9ba6e2fc181192
Deivid Barreto <deividbarreto57@gmail.com>
2024-09-24 15:05:46 -0300

    rotas e criação do objeto com os dados dos projetos

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: HomeComponent,
+        title: 'Portfólio | Deivid Barreto'
+    },
+    {
+        path: '**',
+        loadComponent: () => import('./pages/error/error.component').then(m => m.ErrorComponent),
+        title: 'Page not found'
+    }
+];
---
+++ src/assets/data/projects.ts
@@ -0,0 +1,107 @@
+interface IProjects {
+    name: string,
+    description: string,
+    pathImg: string,
+    techs: Array<string>,
+    linkGitHub: string,
+    linkProject: string
+}
+
+export const projects: Array<IProjects> = [
+    {
+        "name": "Barretos Barbearia",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis illum sapiente incidunt dolorem rem ab praesentium itaque!",
+        "pathImg": "caminho da imagem",
+        "techs": ["html", "css", "js", "react"],
+        "linkGitHub": "",
+        "linkProject": ""
+    },
+    {
+        "name": "AMCK Planejados",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis illum sapiente incidunt dolorem rem ab praesentium itaque!",
+        "pathImg": "caminho da imagem",
+        "techs": ["html", "css", "js"],
+        "linkGitHub": "",
+        "linkProject": ""
+    },
+    {
+        "name": "Restaurante Dining",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis illum sapiente incidunt dolorem rem ab praesentium itaque!",
+        "pathImg": "caminho da imagem",
+        "techs": ["html", "css", "js"],
+        "linkGitHub": "",
+        "linkProject": ""
+    },
+    {
+        "name": "47 Cafeteria",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis illum sapiente incidunt dolorem rem ab praesentium itaque!",
+        "pathImg": "caminho da imagem",
+        "techs": ["html", "css", "js"],
+        "linkGitHub": "",
+        "linkProject": ""
+    },
+    {
+        "name": "Timbrehits",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis illum sapiente incidunt dolorem rem ab praesentium itaque!",
+        "pathImg": "caminho da imagem",
+        "techs": ["html", "css", "js"],
+        "linkGitHub": "",
+        "linkProject": ""
+    },
+    {
+        "name": "Barros Consultores",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis illum sapiente incidunt dolorem rem ab praesentium itaque!",
+        "pathImg": "caminho da imagem",
+        "techs": ["html", "css", "js"],
+        "linkGitHub": "",
+        "linkProject": ""
+    },
+    {
+        "name": "LD Obras",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis illum sapiente incidunt dolorem rem ab praesentium itaque!",
+        "pathImg": "caminho da imagem",
+        "techs": ["html", "css", "js"],
+        "linkGitHub": "",
+        "linkProject": ""
+    },
+    {
+        "name": "Carpintaria",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis illum sapiente incidunt dolorem rem ab praesentium itaque!",
+        "pathImg": "caminho da imagem",
+        "techs": ["html", "css", "js"],
+        "linkGitHub": "",
+        "linkProject": ""
+    },
+    {
+        "name": "Carros Vintage",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis illum sapiente incidunt dolorem rem ab praesentium itaque! ",
+        "pathImg": "caminho da imagem",
+        "techs": ["html", "css", "js"],
+        "linkGitHub": "",
+        "linkProject": ""
+    },
+    {
+        "name": "Harvard",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis illum sapiente incidunt dolorem rem ab praesentium itaque!",
+        "pathImg": "caminho da imagem",
+        "techs": ["html", "css", "js"],
+        "linkGitHub": "",
+        "linkProject": ""
+    },
+    {
+        "name": "Fitness GYM",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis illum sapiente incidunt dolorem rem ab praesentium itaque!",
+        "pathImg": "caminho da imagem",
+        "techs": ["html", "css", "js"],
+        "linkGitHub": "",
+        "linkProject": ""
+    },
+    {
+        "name": "Advice Generator",
+        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis illum sapiente incidunt dolorem rem ab praesentium itaque!",
+        "pathImg": "caminho da imagem",
+        "techs": ["html", "css", "js"],
+        "linkGitHub": "",
+        "linkProject": ""
+    },
+]
