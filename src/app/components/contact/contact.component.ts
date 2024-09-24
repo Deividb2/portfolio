838cb6591a0a741b4aea373fb59c6e471bfbcd8e
Deivid Barreto <deividbarreto57@gmail.com>
2024-09-24 15:06:16 -0300

    remasterização do componente de contato

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,16 +1,19 @@
 import { Component } from '@angular/core';
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
+
+import { radixGithubLogo as gitHub, radixLinkedinLogo as linkedin } from '@ng-icons/radix-icons';
+import { tablerMail as email, tablerBrandWhatsapp as whatsapp } from '@ng-icons/tabler-icons';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [NgIconComponent],
+  viewProviders: [provideIcons({
+    gitHub, email, linkedin, whatsapp
+  })],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
-  onSubmit(event: Event) {
-    event.preventDefault()
 
-    alert("Sabia que vc ia clicar aí. Ainda não programei isso, gatinha")
-  }
 }
