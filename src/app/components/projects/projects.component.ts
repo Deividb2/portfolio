8c2792cb6d2dec87c3cf24e98d0682e968e580a1
Deivid Barreto <deividbarreto57@gmail.com>
2024-09-24 15:04:35 -0300

    correções e estilização da seção de projetos

--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,12 +1,22 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { projects } from '../../../assets/data/projects';
+
+// icons
+import { NgIconComponent, provideIcons } from '@ng-icons/core';
+import { diHtml5Plain as html, diCss3Plain as css, diJavascriptPlain as js, diAngularjsPlain as angular, diTypescriptPlain as ts } from '@ng-icons/devicon/plain';
+import { tablerBrandReact as react } from '@ng-icons/tabler-icons';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [NgIconComponent, CommonModule],
+  viewProviders: provideIcons({
+    html, css, js, ts, angular, react
+  }),
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
 export class ProjectsComponent {
-
+  Projects = projects
 }
