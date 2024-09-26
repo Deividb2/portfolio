9f5979788938664f0d0c93281fabdf4646e14d6f
Deivid Barreto <deividbarreto57@gmail.com>
2024-09-26 16:41:14 -0300

    fazendo o redirect dos links dos projetos para o github e deploy. E add algumas imagens aos projetos

--- src/assets/data/projects.ts
+++ src/assets/data/projects.ts
@@ -12,79 +12,79 @@ export const projects: Array<IProjects> = [
         "name": "Barretos Barbearia",
         "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis illum sapiente incidunt dolorem rem ab praesentium itaque!",
         "pathImg": "../../../assets/images/barretos-barbearia.png",
-        "techs": ["html", "css", "js", "react"],
-        "linkGitHub": "",
-        "linkProject": ""
+        "techs": ["angular", "ts"],
+        "linkGitHub": "https://github.com/Deividb2/barretos-barbearia",
+        "linkProject": "https://barretos-barbearia.vercel.app"
     },
     {
         "name": "AMCK Planejados",
         "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis illum sapiente incidunt dolorem rem ab praesentium itaque!",
         "pathImg": "../../../assets/images/amck.png",
-        "techs": ["html", "css", "js"],
-        "linkGitHub": "",
-        "linkProject": ""
+        "techs": ["html", "css", "js", "angular"],
+        "linkGitHub": "https://github.com/Deividb2/amck",
+        "linkProject": "https://amck-planejados.vercel.app"
     },
     {
         "name": "Restaurante Dining",
         "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis illum sapiente incidunt dolorem rem ab praesentium itaque!",
-        "pathImg": "../../../assets/images/barretos-barbearia.png",
-        "techs": ["html", "css", "js"],
-        "linkGitHub": "",
-        "linkProject": ""
+        "pathImg": "../../../assets/images/restaurante-dining.png",
+        "techs": ["html", "css", "js", "react"],
+        "linkGitHub": "https://github.com/Deividb2/restaurante-dining",
+        "linkProject": "https://restaurante-dining.vercel.app/"
     },
     {
         "name": "47 Cafeteria",
         "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis illum sapiente incidunt dolorem rem ab praesentium itaque!",
-        "pathImg": "../../../assets/images/barretos-barbearia.png",
-        "techs": ["html", "css", "js"],
-        "linkGitHub": "",
-        "linkProject": ""
+        "pathImg": "../../../assets/images/cafeteria.png",
+        "techs": ["html", "css", "js", "react"],
+        "linkGitHub": "https://github.com/Deividb2/cafeteria",
+        "linkProject": "https://cafeteria-murex.vercel.app"
     },
     {
         "name": "Timbrehits",
         "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis illum sapiente incidunt dolorem rem ab praesentium itaque!",
-        "pathImg": "../../../assets/images/barretos-barbearia.png",
-        "techs": ["html", "css", "js"],
-        "linkGitHub": "",
-        "linkProject": ""
+        "pathImg": "../../../assets/images/timbrehits.png",
+        "techs": ["html", "css", "js", "angular"],
+        "linkGitHub": "https://github.com/Deividb2/timbrehits",
+        "linkProject": "https://timbrehits.vercel.app"
     },
     {
         "name": "Barros Consultores",
         "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis illum sapiente incidunt dolorem rem ab praesentium itaque!",
-        "pathImg": "../../../assets/images/barretos-barbearia.png",
-        "techs": ["html", "css", "js"],
-        "linkGitHub": "",
-        "linkProject": ""
+        "pathImg": "../../../assets/images/barros-consultores.png",
+        "techs": ["html", "css", "js", "angular"],
+        "linkGitHub": "https://github.com/Deividb2/barros-consultores",
+        "linkProject": "https://barros-consultores.vercel.app"
     },
     {
         "name": "LD Obras",
         "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis illum sapiente incidunt dolorem rem ab praesentium itaque!",
         "pathImg": "../../../assets/images/barretos-barbearia.png",
-        "techs": ["html", "css", "js"],
-        "linkGitHub": "",
-        "linkProject": ""
+        "techs": ["html", "css", "js", "react"],
+        "linkGitHub": "https://github.com/Deividb2/ld-obras",
+        "linkProject": "https://ld-obras.vercel.app"
     },
     {
         "name": "Carpintaria",
         "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis illum sapiente incidunt dolorem rem ab praesentium itaque!",
         "pathImg": "../../../assets/images/barretos-barbearia.png",
-        "techs": ["html", "css", "js"],
-        "linkGitHub": "",
-        "linkProject": ""
+        "techs": ["html", "css", "js", "react"],
+        "linkGitHub": "https://github.com/Deividb2/carpintaria",
+        "linkProject": "https://carpintaria.vercel.app"
     },
     {
         "name": "Carros Vintage",
         "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis illum sapiente incidunt dolorem rem ab praesentium itaque! ",
         "pathImg": "../../../assets/images/barretos-barbearia.png",
-        "techs": ["html", "css", "js"],
-        "linkGitHub": "",
+        "techs": ["html", "css"],
+        "linkGitHub": "https://github.com/Deividb2/carros-vintage",
         "linkProject": ""
     },
     {
         "name": "Harvard",
         "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis illum sapiente incidunt dolorem rem ab praesentium itaque!",
         "pathImg": "../../../assets/images/barretos-barbearia.png",
-        "techs": ["html", "css", "js"],
+        "techs": ["html", "css", "js", "angular"],
         "linkGitHub": "",
         "linkProject": ""
     },
@@ -92,16 +92,16 @@ export const projects: Array<IProjects> = [
         "name": "Fitness GYM",
         "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis illum sapiente incidunt dolorem rem ab praesentium itaque!",
         "pathImg": "../../../assets/images/barretos-barbearia.png",
-        "techs": ["html", "css", "js"],
-        "linkGitHub": "",
-        "linkProject": ""
+        "techs": ["html", "css", "js", "react"],
+        "linkGitHub": "https://github.com/Deividb2/fitness_gym",
+        "linkProject": "https://fitness-gym-two.vercel.app"
     },
     {
         "name": "Advice Generator",
         "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis illum sapiente incidunt dolorem rem ab praesentium itaque!",
-        "pathImg": "../../../assets/images/barretos-barbearia.png",
-        "techs": ["html", "css", "js"],
-        "linkGitHub": "",
-        "linkProject": ""
+        "pathImg": "../../../assets/images/advice-generator.png",
+        "techs": ["html", "css", "js", "react"],
+        "linkGitHub": "https://github.com/Deividb2/advice-generator",
+        "linkProject": "https://advice-generator-pi-vert.vercel.app"
     },
 ]
