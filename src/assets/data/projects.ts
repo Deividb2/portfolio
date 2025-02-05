6d411ebbfaf3487a4e9d3455c97c1c49d2303d54
Deividb2 <deividbarreto57@gmail.com>
2025-02-05 12:22:28 -0300

    new commit

--- src/assets/data/projects.ts
+++ src/assets/data/projects.ts
@@ -2,99 +2,51 @@ import { IProjects } from "../interfaces/projects.interface";
 
 export const projects: Array<IProjects> = [
     {
-        "name": "Barretos Barbearia",
-        "description": "Este é o projeto de uma landing page que divulga os serviços de uma barbearia. Nela se pode encontrar as diversas informações como localização, contato e serviços.",
-        "pathImg": "../../../assets/images/barretos-barbearia.png",
-        "techs": ["angular", "ts"],
-        "linkGitHub": "https://github.com/Deividb2/barretos-barbearia",
-        "linkProject": "https://barretos-barbearia.vercel.app"
+        name: "Editor Mobile",
+        description: "O Editor mobile é um site que divulga um curso de edição de vídeos que pode ser feito apenas pelo celular.",
+        pathImg: "../../../assets/images/editor-mobile.png",
+        techs: ["html", "css", "ts", "angular"],
+        linkGitHub: "",
+        linkProject: "https://editor-mobile.vercel.app/"
     },
     {
-        "name": "AMCK Planejados",
-        "description": "A AMCK Planejados é uma empresa fabricante de móveis planejados. Neste projeto, desenvolvi uma landing page no qual divulga os seus serviços.",
-        "pathImg": "../../../assets/images/amck.png",
-        "techs": ["html", "css", "js", "angular"],
-        "linkGitHub": "https://github.com/Deividb2/amck",
-        "linkProject": "https://amck-planejados.vercel.app"
+        name: "Barretos Academia",
+        description: "Este projeto é uma landing page que divulga serviços de uma Barbearia.",
+        pathImg: "../../../assets/images/barretos-barbearia.png",
+        techs: ["html", "css", "js", "angular"],
+        linkGitHub: "https://github.com/Deividb2/fitness_gym",
+        linkProject: "https://fitness-gym-two.vercel.app"
     },
     {
-        "name": "Restaurante Dining",
-        "description": "Este é um projeto de um restaurante localizado na beira de um lago que contém diversas páginas como página de contato, cardápio e promoções do dia.",
-        "pathImg": "../../../assets/images/restaurante-dining.png",
-        "techs": ["html", "css", "js", "react"],
-        "linkGitHub": "https://github.com/Deividb2/restaurante-dining",
-        "linkProject": "https://restaurante-dining.vercel.app/"
+        name: "Barretos Barbearia",
+        description: "Barreto Barbearia projeto que promove os serviços estéticos de uma barbearia.",
+        pathImg: "../../../assets/images/barretos-barbearia.png",
+        techs: ["angular", "ts"],
+        linkGitHub: "https://github.com/Deividb2/barretos-barbearia",
+        linkProject: "https://barretos-barbearia.vercel.app"
     },
     {
-        "name": "47 Cafeteria",
-        "description": "Neste projeto desenvolvi uma cafeteria com diversas páginas.",
-        "pathImg": "../../../assets/images/cafeteria.png",
-        "techs": ["html", "css", "js", "react"],
-        "linkGitHub": "https://github.com/Deividb2/cafeteria",
-        "linkProject": "https://cafeteria-murex.vercel.app"
+        name: "AMCK Planejados",
+        description: "A AMCK Planejados é uma empresa fabricante de móveis planejados. Neste projeto, desenvolvi uma landing page no qual divulga os seus serviços.",
+        pathImg: "../../../assets/images/amck.png",
+        techs: ["html", "css", "js", "angular"],
+        linkGitHub: "https://github.com/Deividb2/amck",
+        linkProject: "https://amck-planejados.vercel.app"
     },
     {
-        "name": "Timbrehits",
-        "description": "Nessa projeto, desenvolvi um site que divulga um fone de ouvido de alta qualidade. Nele está presente 3 páginas principais: Home, Benefícios e Depoimentos",
-        "pathImg": "../../../assets/images/timbrehits.png",
-        "techs": ["html", "css", "js", "angular"],
-        "linkGitHub": "https://github.com/Deividb2/timbrehits",
-        "linkProject": "https://timbrehits.vercel.app"
+        name: "Restaurante Dining",
+        description: "Este é um projeto de um restaurante localizado na beira de um lago que contém diversas páginas como página de contato, cardápio e promoções do dia.",
+        pathImg: "../../../assets/images/restaurante-dining.png",
+        techs: ["html", "css", "js", "react"],
+        linkGitHub: "https://github.com/Deividb2/restaurante-dining",
+        linkProject: "https://restaurante-dining.vercel.app/"
     },
     {
-        "name": "Barros Consultores",
-        "description": "Barros Consultores é uma empresa de gestão de negócios. Aqui eu desenolvi uma landing page que mostra os seus serviços.",
-        "pathImg": "../../../assets/images/barros-consultores.png",
-        "techs": ["html", "css", "js", "angular"],
-        "linkGitHub": "https://github.com/Deividb2/barros-consultores",
-        "linkProject": "https://barros-consultores.vercel.app"
-    },
-    {
-        "name": "LD Obras",
-        "description": "Este é um site que desenvolvi pensando na LD Obras, que é um empresa prestadora de serviços de pintura, alvenaria e reformas. Nele está toda a divulgação da empresa.",
-        "pathImg": "../../../assets/images/ld-obras.png",
-        "techs": ["html", "css", "js", "react"],
-        "linkGitHub": "https://github.com/Deividb2/ld-obras",
-        "linkProject": "https://ld-obras.vercel.app"
-    },
-    {
-        "name": "Carpintaria",
-        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis illum sapiente incidunt dolorem rem ab praesentium itaque!",
-        "pathImg": "../../../assets/images/barretos-barbearia.png",
-        "techs": ["html", "css", "js", "react"],
-        "linkGitHub": "https://github.com/Deividb2/carpintaria",
-        "linkProject": "https://carpintaria.vercel.app"
-    },
-    {
-        "name": "Carros Vintage",
-        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis illum sapiente incidunt dolorem rem ab praesentium itaque! ",
-        "pathImg": "../../../assets/images/barretos-barbearia.png",
-        "techs": ["html", "css"],
-        "linkGitHub": "https://github.com/Deividb2/carros-vintage",
-        "linkProject": ""
-    },
-    {
-        "name": "Harvard",
-        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis illum sapiente incidunt dolorem rem ab praesentium itaque!",
-        "pathImg": "../../../assets/images/barretos-barbearia.png",
-        "techs": ["html", "css", "js", "angular"],
-        "linkGitHub": "",
-        "linkProject": ""
-    },
-    {
-        "name": "Fitness GYM",
-        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis illum sapiente incidunt dolorem rem ab praesentium itaque!",
-        "pathImg": "../../../assets/images/barretos-barbearia.png",
-        "techs": ["html", "css", "js", "react"],
-        "linkGitHub": "https://github.com/Deividb2/fitness_gym",
-        "linkProject": "https://fitness-gym-two.vercel.app"
-    },
-    {
-        "name": "Advice Generator",
-        "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis illum sapiente incidunt dolorem rem ab praesentium itaque!",
-        "pathImg": "../../../assets/images/advice-generator.png",
-        "techs": ["html", "css", "js", "react"],
-        "linkGitHub": "https://github.com/Deividb2/advice-generator",
-        "linkProject": "https://advice-generator-pi-vert.vercel.app"
-    },
+        name: "Timbrehits",
+        description: "Nessa projeto, desenvolvi um site que divulga um fone de ouvido de alta qualidade. Nele está presente 3 páginas principais: Home, Benefícios e Depoimentos",
+        pathImg: "../../../assets/images/timbrehits.png",
+        techs: ["html", "css", "js", "angular"],
+        linkGitHub: "https://github.com/Deividb2/timbrehits",
+        linkProject: "https://timbrehits.vercel.app"
+    }
 ]
